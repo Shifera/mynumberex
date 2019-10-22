6a119feb65c5ccf0057f03d000baf2feae101f9c
Shifera <shiferawregassa@gmail.com>
2019-10-22 10:19:36 -0400

    ok

---
+++ src/mynumberex.java
@@ -0,0 +1,13 @@
+/*
+Write an application that assigns 2 to a variable
+named mynumber and 1.7938 to a variable namedmyothernumber,
+and then prints out the values to the console.
+ */
+public class mynumberex {
+    public static void main (String args[]){
+        int mynumber = 2;
+        double myothernumber = 1.7938;
+        System.out.println("mynumb is " + mynumber);
+        System.out.println("the value of myothernumb is " + myothernumber);
+    }
+}
